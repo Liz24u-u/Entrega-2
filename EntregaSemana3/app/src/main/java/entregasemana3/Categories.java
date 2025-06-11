1b05cc95038fd4e491ec22697783915f7abc4e90
Liz24u-u <pinillaangie36@gmail.com>
2025-06-10 22:06:53 -0500

    Update Categories.java

--- EntregaSemana3/app/src/main/java/entregasemana3/Categories.java
+++ EntregaSemana3/app/src/main/java/entregasemana3/Categories.java
@@ -1,10 +1,26 @@
 package entregasemana3;
-
+/**
+ * Enum que representa diferentes categorías utilizadas para clasificar frases o vocabulario.
+ * Cada categoría tiene una descripción asociada.
+ */
 public enum Categories {
+     /** 
+     * Categoría relacionada con aspectos de la personalidad de las personas. 
+     */
     PERSONAL("aspectos de personalidad de las personas"),
+      /** 
+     * Categoría relacionada con defectos o fallas en objetos. 
+     */
     OBJECTS("defectos en objetos"),
+     /** 
+     * Categoría relacionada con el vocabulario del ámbito de los negocios. 
+     */
     BUSSINESS("vocabulario de negocios"),
+    /** 
+     * Categoría relacionada con términos del ámbito de la programación. 
+     */
     PROGRAMMING("vocabulario de programación");
+    // Descripción asociada a cada categoría
 
     Categories(String phrase) {
 
