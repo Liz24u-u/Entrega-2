1498a9755990415b9cdb729afaf77dff973a8f4e
Liz24u-u <pinillaangie36@gmail.com>
2025-06-10 22:26:31 -0500

    Update Player.java

--- EntregaSemana3/app/src/main/java/entregasemana3/Player.java
+++ EntregaSemana3/app/src/main/java/entregasemana3/Player.java
@@ -1,9 +1,21 @@
 package entregasemana3;
-
+/**
+ * Clase que representa a un jugador del juego.
+ * Contiene la información básica del usuario como nombre, contraseña y su puntuación.
+ */
 public class Player {
+        /** Puntuación del jugador. */
     private RecordScore score;
+        /** Nombre de usuario del jugador. */
     private String userName;
+        /** Contraseña del jugador. */
     private String passWord;
-
+/**
+     * Constructor de la clase Player.
+     * Crea un nuevo jugador con nombre de usuario y contraseña especificados.
+     *
+     * @param userName Nombre de usuario del jugador.
+     * @param passWord Contraseña del jugador.
+     */
     public Player(String userName, String passWord)
 }
